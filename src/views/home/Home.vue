e4f1de0ae9e844c312f6c70748dd28523c9fd4df
roboslyq <robosloveu@163.com>
2020-02-03 11:24:46 +0800

    欢迎页面数据加载

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,12 +1,12 @@
 <template>
-  <el-table :data="tableData" border style="width: 100%">
-    <el-table-column fixed prop="date" label="日期" width="150"></el-table-column>
-    <el-table-column prop="name" label="姓名" width="120"></el-table-column>
-    <el-table-column prop="province" label="省份" width="120"></el-table-column>
-    <el-table-column prop="city" label="市区" width="120"></el-table-column>
-    <el-table-column prop="address" label="地址" width="300"></el-table-column>
-    <el-table-column prop="zip" label="邮编" width="120"></el-table-column>
-    <el-table-column fixed="right" label="操作" width="100">
+  <el-table :data="tableData"  border stripe  style="width: 100%">
+    <el-table-column fixed prop="birthday" label="日期" width="100%"></el-table-column>
+    <el-table-column prop="name" label="姓名" width="100%"></el-table-column>
+    <el-table-column prop="province" label="省份" width="100%"></el-table-column>
+    <el-table-column prop="city" label="市区" width="100%"></el-table-column>
+    <el-table-column prop="address" label="地址" width="300%"></el-table-column>
+    <el-table-column prop="zip" label="邮编" width="100%"></el-table-column>
+    <el-table-column  label="操作" width="100%">
       <template slot-scope="scope">
         <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
         <el-button type="text" size="small">编辑</el-button>
@@ -16,7 +16,21 @@
 </template>
 
 <script>
+
 export default {
+    created() {
+        console.log('进入页面初始化')
+        // debugger
+        this.isShowLoading = true
+        this.$axios.get('/user/findAll')
+        .then(response => {
+            console.log(response)
+            this.tableData = response
+        }).catch(error => {
+            console.log(error)
+            this.isShowLoading = false
+        })
+    },
     methods: {
         handleClick(row) {
             console.log(row)
@@ -24,16 +38,7 @@ export default {
     },
     data() {
         return {
-            tableData: [
-                {
-                    date: '2016-05-02',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333,
-                },
-            ],
+            tableData: [],
         }
     },
 }
